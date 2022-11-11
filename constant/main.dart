2ad4c605677022d9b70a830d0f04392b57a61dce
Yoochanhong <chanhongy6@naver.com>
2022-11-12 01:01:46 +0900

    상수

---
+++ constant/main.dart
@@ -0,0 +1,23 @@
+void main(){
+
+  final int num = 10;
+  const int num1 = 10;
+
+  //num = 20;
+  //num1 = 20;
+
+  final char = '홍승재';
+  const char1 = '이정호';
+
+  final num2;
+  //const num3;
+
+  final now1 = DateTime.now();
+  //const now2 = DateTime.now();
+
+  print(num);
+  print(num1);
+  print(char);
+  print(char1);
+  print(now1);
+}
