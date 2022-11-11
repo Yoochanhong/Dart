787e9cd8aaea2d3946a9594db3d485bf8a93a191
Yoochanhong <chanhongy6@naver.com>
2022-11-11 23:43:38 +0900

    var와 dynamic

---
+++ variable/main.dart
@@ -0,0 +1,17 @@
+void main(){
+
+  String name = '유찬홍';
+
+  var friend = '조병진';
+
+  //friend = 17;
+
+  dynamic friend2 = '박준수';
+
+  friend2 = 11;
+
+
+  print(name);
+  print(friend);
+  print(friend2);
+}
